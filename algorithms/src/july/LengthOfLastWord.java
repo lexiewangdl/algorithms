400b89005b20612d9ff43aeb8832529a3e0561e3
lexiewangdl <travelaroundthemoon@gmail.com>
2022-07-05 17:23:07 +0800

    58 length of last word

--- algorithms/src/july/LengthOfLastWord.java
+++ algorithms/src/july/LengthOfLastWord.java
@@ -2,6 +2,21 @@ package july;
 
 public class LengthOfLastWord {
 	
+	// 内存消耗更少
+    public int lengthLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+
+	
     public int lengthOfLastWord(String s) {
 
     	String[] splitted = s.split(" ");
