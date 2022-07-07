4d9cb9e1068abb42066e21cd2d29451ade8e7f65
lexiewangdl <travelaroundthemoon@gmail.com>
2022-07-07 16:56:51 +0800

    63 Unique Paths II 执行用时 0ms 在所有Java提交中击败了100%的用户

---
+++ algorithms/src/july/UniquePathsII.java
@@ -0,0 +1,41 @@
+package july;
+
+public class UniquePathsII {
+	
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    	
+    	int nrows = obstacleGrid.length;
+    	int ncols = obstacleGrid[0].length;
+
+    	int[][] dp = new int[nrows][ncols];
+    	
+    	boolean obs = false;
+    	for (int i = 0; i < nrows; i++) {
+    		if (obstacleGrid[i][0] == 1) {
+    			obs = true;
+    		}
+    		dp[i][0] = (obs) ? 0 : 1;
+    	}
+    	
+    	obs = false;
+    	for (int i = 0; i < ncols; i++) {
+    		if (obstacleGrid[0][i] == 1) {
+    			obs = true;
+    		}
+    		dp[0][i] = (obs) ? 0 : 1;
+    	}
+    	
+    	for (int i = 1; i < nrows; i++) {
+    		for (int j = 1; j < ncols; j++) {
+    			if (obstacleGrid[i][j] == 1) {
+    				dp[i][j] = 0;
+    			} else {
+    				dp[i][j] = dp[i][j-1] + dp[i-1][j]; // ¶¯Ì¬·½³Ì
+    			}
+    		}
+    	}
+    	
+    	return dp[nrows-1][ncols-1];
+    }
+
+}
