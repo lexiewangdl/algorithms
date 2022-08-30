3753e37c00c873bb1de997d25668bee20e001bee
lexiewangdl <travelaroundthemoon@gmail.com>
2022-08-30 22:31:21 +0800

    101 Symmetric Tree; Recursion

--- algorithms/src/august/SymmetricTree.java
+++ algorithms/src/august/SymmetricTree.java
@@ -21,10 +21,18 @@ public class SymmetricTree {
 	 }
 	    
 	 public boolean solve(TreeNode left, TreeNode right) {
+		 
+		 // If both left node and right node are null, the two are symmetric
+		 // We've reached end of tree
 		 if (left == null && right == null) return true;
+		 
+		 // If either left or right is null, NOT symmetric
 		 else if (left == null || right == null) return false;
+		 
+		 // If values don't equal, NOT symmetric
 		 if (left.val != right.val) return false;
 		 
+		 // Do the same for left's children and right's children
 		 return solve(left.left, right.right) && solve(left.right, right.left);
 	 }
 
